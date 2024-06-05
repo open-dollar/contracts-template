753a0160b4f959de54ac777b6e76a41a18198cfb
Hunter King <hking6776@gmail.com>
2024-06-04 18:04:34 -0600

    this compiles

--- src/contracts/Test.sol
+++ src/contracts/Test.sol
@@ -1,18 +1,15 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.20;
 
-import {IERC20} from '@openzeppelin/token/ERC20/ERC20.sol';
-
-import {ISAFEEngine} from '@opendollar/contracts/SAFEEngine.sol';
-import {IVault721} from '@opendollar/interfaces/proxies/IVault721.sol';
 import {Math} from '@opendollar/libraries/Math.sol';
+import {ITest} from '@interfaces/ITest.sol';
 
-import {ITest} from '../interfaces/ITest.sol';
-
-contract Test is IERC20, ISAFEEngine, IVault721, ITest {
+contract Test is ITest {
   using Math for uint256;
 
   constructor() {}
 
-  function foo() external view {}
+  function getNum() external pure returns (uint256 _num) {
+    _num = 1;
+  }
 }
